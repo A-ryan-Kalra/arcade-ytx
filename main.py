1c7c0be127e3cdb4487360a3f799409a6e6a6fe4
Aryan Kalra <aryan.smart832@gmail.com>
2025-06-27 16:35:54 +0530

    changes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from rock_paper_scissors import rock_paper_scissor
-from guess_number import guess_number
+from game_01.rock_paper_scissors import rock_paper_scissor
+from game_02.guess_number import guess_number
 
 if __name__ == "__main__":
     import argparse
