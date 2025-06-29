78c3146fb8612403b945812952e7ba5ce3bce328
Aryan Kalra <aryan.smart832@gmail.com>
2025-06-29 09:47:33 +0530

    add delete accounts

--- game_03/main.py
+++ game_03/main.py
@@ -12,14 +12,26 @@ user_data = {}
 
 
 def create_user(user_name, type, initial_amount):
-    if user_name not in user_data:
+    if user_name + type not in user_data:
         if int(type) == 1:
             new_account = SavingsAccount(user_name, float(initial_amount))
         elif int(type) == 2:
             new_account = CurrentAccount(user_name, float(initial_amount))
 
-        user_data[user_name] = new_account
-        return user_data[user_name]
+    elif user_name + type in user_data:
+        if user_data[user_name + type].type == type:
+            raise Exception(
+                "\nUser already created, please try again with another username\n"
+            )
+        else:
+            if int(type) == 1:
+                new_account = SavingsAccount(user_name, float(initial_amount))
+            elif int(type) == 2:
+                new_account = CurrentAccount(user_name, float(initial_amount))
+
+    user_data[user_name + type] = new_account
+
+    return user_data[user_name + type]
 
 
 def get_user(user_name):
@@ -71,35 +83,38 @@ def fetch_info(num, account: Union[BankAccount], store_details):
                     return
 
                 while True:
-                    os.system("cls" if os.name == "nt" else "clear")
-                    # account_create(store_details)
-                    initial_amount, user_name, type = account_create(
-                        store_details
-                    ).values()
-
-                    create_user(user_name, type, initial_amount)
-
-                    # os.system("cls" if os.name == "nt" else "clear")
-
-                    console.print(
-                        "Would you like to add more accounts? (y/n):  ",
-                        style="yellow bold",
-                    )
-                    while True:
-                        add_account = input("")
-                        if add_account.lower() not in ["y", "n"]:
-                            print("\nPlease enter (y/n) \n")
-                            continue
-                        else:
-                            break
-                    if add_account.lower() == "n":
+                    try:
+                        # os.system("cls" if os.name == "nt" else "clear")
+                        # account_create(store_details)
+                        initial_amount, user_name, type = account_create(
+                            store_details
+                        ).values()
                         os.system("cls" if os.name == "nt" else "clear")
-                        all_acounts = [
-                            detail
-                            for detail in store_details
-                            if detail.get("user_name") != account.name
-                        ]
-                        break
+                        create_user(user_name, type, initial_amount)
+
+                        console.print(
+                            "Would you like to add more accounts? (y/n):  ",
+                            style="yellow bold",
+                        )
+                        while True:
+                            add_account = input("")
+                            if add_account.lower() not in ["y", "n"]:
+                                print("\nPlease enter (y/n) \n")
+                                continue
+                            else:
+                                break
+                        if add_account.lower() == "n":
+                            os.system("cls" if os.name == "nt" else "clear")
+                            all_acounts = [
+                                detail
+                                for detail in store_details
+                                if detail.get("user_name") != account.name
+                                or detail.get("type") != account.type
+                            ]
+                            break
+                    except Exception as error:
+                        print(error)
+                        # continue
 
             table.add_column("No", style="blue", justify="center")
             table.add_column("Name", style="cyan")
@@ -111,7 +126,7 @@ def fetch_info(num, account: Union[BankAccount], store_details):
                     str(index + 1) + ".",
                     user.get("user_name"),
                     user.get("type"),
-                    str(user.get("initial_amount")),
+                    f"{user.get("initial_amount"):.2f}",
                 )
 
             console.print(Align.center(table, style="bold"))
@@ -127,7 +142,8 @@ def fetch_info(num, account: Union[BankAccount], store_details):
                     continue
 
             selected_user_name = all_acounts[select_acc - 1].get("user_name")
-            transfer_acc = get_user(user_name=selected_user_name)
+            type = all_acounts[select_acc - 1].get("type")
+            transfer_acc = get_user(user_name=selected_user_name + type)
 
             menu = "\nPlease enter the amount you wish to transfer\n"
             console.print(menu, style="bold magenta")
@@ -142,55 +158,63 @@ def fetch_info(num, account: Union[BankAccount], store_details):
             table.add_column("Type", style="magenta")
             table.add_column("Balance", style="green")
 
-            for index, user in enumerate(user_data):
-                acc = get_user(user_name=user)
+            all_acounts = store_details
+
+            for index, user in enumerate(all_acounts):
                 table.add_row(
                     str(index + 1) + ".",
-                    acc.name,
-                    acc.type,
-                    f"{acc.balance:.2f}",
+                    user.get("user_name"),
+                    user.get("type"),
+                    f"{user.get("initial_amount"):.2f}",
                 )
             console.print(Align.center(table, style="bold"))
             return
         case 6:
             while True:
-                initial_amount, user_name, type = account_create(store_details).values()
-                print("chosen", type)
-                create_user(user_name, type, initial_amount)
+                try:
+                    initial_amount, user_name, type = account_create(
+                        store_details
+                    ).values()
 
-                fetch_info(5, account, store_details)
+                    create_user(user_name, type, initial_amount)
 
-                console.print(
-                    "Would you like to add more accounts? (y/n):  ",
-                    style="yellow bold",
-                )
+                    fetch_info(5, account, store_details)
 
-                while True:
-                    create_again = input("")
-                    if create_again.lower() not in ["y", "n"]:
-                        print("\nPlease enter (y/n) ")
+                    console.print(
+                        "Would you like to add more accounts? (y/n):  ",
+                        style="yellow bold",
+                    )
+
+                    while True:
+                        create_again = input("")
+                        if create_again.lower() not in ["y", "n"]:
+                            print("\nPlease enter (y/n) ")
+                            continue
+                        else:
+                            break
+                    if create_again == "y":
+                        os.system("cls" if os.name == "nt" else "clear")
                         continue
                     else:
+                        os.system("cls" if os.name == "nt" else "clear")
                         break
-                if create_again == "y":
-                    os.system("cls" if os.name == "nt" else "clear")
-                    continue
-                else:
-                    os.system("cls" if os.name == "nt" else "clear")
-                    break
+                except Exception as error:
+                    print(error)
         case 7:
-            fetch_info(5, account, store_details)
-
             all_acounts = [
                 detail
                 for detail in store_details
                 if detail.get("user_name") != account.name
+                or detail.get("type") != account.type
             ]
+
             if len(all_acounts) == 0:
                 print(
                     "Oops, You don't have enough accounts,\nplease create more accounts and try again."
                 )
                 return account
+
+            fetch_info(5, account, store_details=all_acounts)
 
             console.print(
                 "\nPlease select the account via number to switch your account with\n"
@@ -203,18 +227,76 @@ def fetch_info(num, account: Union[BankAccount], store_details):
                 elif switch_acc < 0:
                     print("Please enter number only")
 
-                if switch_acc not in range(0, len(all_acounts) + 1):
+                if switch_acc not in range(0, len(all_acounts)):
                     console.print(
-                        f"\nPlease select the number between (1 - {len(all_acounts)+1})\n",
+                        f"\nPlease select the number between (1 - {len(all_acounts)})\n",
                         style="bold red on black",
                     )
                     continue
                 else:
                     break
-            get_acc = all_acounts[switch_acc - 1]
+
+            get_acc = all_acounts[switch_acc]
+
             user_name = get_acc["user_name"]
-            account = user_data[user_name]
+            type = 1 if get_acc["type"] == "Savings" else 2
+            account = user_data[user_name + str(type)]
             return account
+        case 8:
+            all_acounts = [
+                detail
+                for detail in store_details
+                if detail.get("user_name") != account.name
+                or detail.get("type") != account.type
+            ]
+
+            if len(all_acounts) == 0:
+                print(
+                    "Oops, You don't have enough accounts,\nplease create more accounts and try again."
+                )
+                return store_details
+            table = Table(title="List Of Accounts", min_width=50)
+            table.add_column("No", style="blue", justify="center")
+            table.add_column("Name", style="cyan")
+            table.add_column("Type", style="magenta")
+            table.add_column("Balance", style="green")
+
+            for index, user in enumerate(all_acounts):
+                table.add_row(
+                    str(index + 1) + ".",
+                    user.get("user_name"),
+                    user.get("type"),
+                    f"{user.get("initial_amount"):.2f}",
+                )
+            console.print(Align.center(table, style="bold"))
+            console.print(
+                "\nPlease select the account via number to delete the account\n"
+            )
+            while True:
+                switch_acc = int(input("\nYour choice\n")) - 1
+
+                if not switch_acc.is_integer():
+                    print("Please enter number only")
+                elif switch_acc < 0:
+                    print("Please enter number only")
+
+                if switch_acc not in range(0, len(all_acounts)):
+                    console.print(
+                        f"\nPlease select the number between (1 - {len(all_acounts)})\n",
+                        style="bold red on black",
+                    )
+                    continue
+                else:
+                    break
+            get_acc = all_acounts[switch_acc]
+
+            user_name = get_acc["user_name"]
+            store_details = [
+                detail for detail in store_details if detail["user_name"] != user_name
+            ]
+            type = 1 if get_acc["type"] == "Savings" else 2
+            del user_data[user_name + str(type)]
+            return store_details
 
 
 def account_create(store_details: list) -> dict:
@@ -243,23 +325,34 @@ def account_create(store_details: list) -> dict:
             end_loop = False
 
     console.print("\nPlease enter name", style="bold magenta")
-    user_name = input()
+    user_name = input().strip()
+
+    type = "Savings" if int(user_choice) == 1 else "Current"
+    if user_name + user_choice in user_data:
+
+        if user_data[user_name + user_choice].type == type:
+            raise Exception(
+                "\nUser already created, please try again with another username\n"
+            )
+
     console.print(
         "\nPlease enter the intial amount you want to deposit", style="bold yellow"
     )
 
     while True:
         try:
-            initial_amount = float(input())
+            initial_amount = float(input().strip())
             break
         except ValueError:
             console.print(
                 "Invalid input. Please enter a number: ", style="bold red on black"
             )
-    type = "Savings" if int(user_choice) == 1 else "Current"
+
     store_details.append(
         {
-            "initial_amount": initial_amount,
+            "initial_amount": (
+                initial_amount * 1.05 if int(user_choice) == 1 else initial_amount
+            ),
             "user_name": user_name,
             "type": type,
         }
@@ -286,10 +379,10 @@ def bank_account_main():
         elif int(type) == 2:
             account = CurrentAccount(user_name, float(initial_amount))
 
-        user_data[user_name] = account
+        user_data[user_name + type] = account
 
         while True:
-            menu = "\n1. Show Balance\t\t2. Deposit Amount\n\n3. Withdraw Amount\t4. Transfer Amount\n\n5. Show All Acounts\t6. Create Account\n\n7. Switch Account\t8. Exit"
+            menu = "\n1. Show Balance\t\t2. Deposit Amount\n\n3. Withdraw Amount\t4. Transfer Amount\n\n5. Show All Acounts\t6. Create Account\n\n7. Switch Account\t8. Delete Accounts\n\n9. Exit"
 
             console.print(
                 Panel(
@@ -305,9 +398,9 @@ def bank_account_main():
             )
             while True:
                 user_choice = int(input("\nYour choice: "))
-                if user_choice not in range(1, 9):
+                if user_choice not in range(1, 10):
                     console.print(
-                        "You must enter 1,2,3,4,5,6,7 or 8", style="bold red on black"
+                        "You must enter 1,2,3,4,5,6,7,8 or 9", style="bold red on black"
                     )
                     continue
                 else:
@@ -320,6 +413,12 @@ def bank_account_main():
             elif entered_choice == 7:
                 account = fetch_info(entered_choice, account, store_details)
             elif entered_choice == 8:
+                store_details = fetch_info(entered_choice, account, store_details)
+                # if len(store_details) == 0:
+                #     os.system("cls" if os.name == "nt" else "clear")
+                #     bank_account_main()()
+                #     break
+            elif entered_choice == 9:
                 break
             else:
                 console.print(
